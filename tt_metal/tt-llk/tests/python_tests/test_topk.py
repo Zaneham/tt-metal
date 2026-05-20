266d797bdc374da3817087bed68650ac6a0a2e52
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2026-05-20 09:20:29 +0000

    remove topk skip for WH

--- tt_metal/tt-llk/tests/python_tests/test_topk.py
+++ tt_metal/tt-llk/tests/python_tests/test_topk.py
@@ -27,6 +27,7 @@ import sys
 
 import pytest
 import torch
+from helpers.chip_architecture import ChipArchitecture, get_chip_architecture
 from helpers.format_config import DataFormat, InputOutputFormat
 from helpers.golden_generators import (
     ELEMENTS_PER_TILE,
@@ -289,7 +290,10 @@ def test_topk_sfpu(
     stable_sort: bool,
 ):
 
-    if input_dimensions == [32, 1024]:
+    if (
+        input_dimensions == [32, 1024]
+        and get_chip_architecture() == ChipArchitecture.BLACKHOLE
+    ):
         # For 32x1024 input on blackhole arch, we have observed some discrepancies in the topk values between hardware and golden.
         # TODO: Fix issue #1344 on tt-llk.
         pytest.skip(
